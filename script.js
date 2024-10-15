a04ee989e9e3a44d3e2712fe9fd2e0a08872cacb
Ishtiaque Ahmed Khan <ishtiaque.niloy@gmail.com>
2024-10-15 19:02:08 +0000

    learning while loop.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const money = 100;
+let upper = 10000;
+let randomNumber = getRandomNumber(upper);
+let guess;
+let attempts = 0;
 
-if(money){
-    console.log("Don't spend it all.");
-}else{
-    console.log("You should get a job.");    
+function getRandomNumber(upper){
+    return Math.floor(Math.random() * upper) + 1;
 }
+
+while(guess !== randomNumber){
+    guess = getRandomNumber(upper);
+    attempts++;
+}
+
+document.write("<p>The random number was: " + randomNumber + "</p>")
+document.write("<p>It took computer to : " + attempts + " attempts to get it right.")
