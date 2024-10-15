a16fb2eb8a71e22e5e7c8f82b047f78b85f46da2
Ishtiaque Ahmed Khan <ishtiaque.niloy@gmail.com>
2024-10-15 21:29:55 +0000

    add eventlistener

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let upper = 10000;
-let randomNumber = getRandomNumber(upper);
-let guess;
-let attempts = 0;
+const myHeader = document.getElementById('myHeading')
 
-function getRandomNumber(upper){
-    return Math.floor(Math.random() * upper) + 1;
-}
-
-while(guess !== randomNumber){
-    guess = getRandomNumber(upper);
-    attempts++;
-}
-
-document.write("<p>The random number was: " + randomNumber + "</p>")
-document.write("<p>It took computer to : " + attempts + " attempts to get it right.")
+myHeader.addEventListener('click', ()=>{
+    myHeader.style.color="red";
+})
