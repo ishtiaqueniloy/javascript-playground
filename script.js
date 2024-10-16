14ce9d49434785b5ac99ac4b4e34f89e5d310294
Ishtiaque Ahmed Khan <ishtiaque.niloy@gmail.com>
2024-10-16 00:01:20 +0000

    selecting page element by id

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const myHeader = document.getElementById('myHeading')
+const myButton = document.getElementById('myButton')
+const myTextInput = document.getElementById('myTextInput')
+const myResetButton = document.getElementById('myResetButton')
 
-myHeader.addEventListener('click', ()=>{
-    myHeader.style.color="red";
+// myHeader.addEventListener('click', ()=>{
+//     myHeader.style.color="red";
+// })
+
+myButton.addEventListener('click', ()=>{
+    myHeader.style.color=myTextInput.value;
+})
+
+myResetButton.addEventListener('click', ()=>{
+    myHeader.style.color="black";
 })
